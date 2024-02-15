f31bff7dafcd30f0f35d5440c37cb6e11fad3dd8
Kaung Sett Hein <43985555+KaungSettHein@users.noreply.github.com>
2024-02-15 01:51:51 +0000

    Type script exercise started

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,6 +1,21 @@
-var character = "no one";
+"use strict";
+const character = "no one";
 console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
+const inputs = document.querySelectorAll('input');
+inputs.forEach(input => {
     console.log(input);
 });
+let greet;
+greet = () => {
+    console.log("Hi Mom!");
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(1, 2);
+const addition = (a, b) => {
+    return a + b;
+};
+let value = addition(10, 1);
+console.log(value);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -5,3 +5,24 @@ const inputs = document.querySelectorAll('input');
 inputs.forEach(input => {
     console.log(input);
 })
+
+let greet: Function;
+
+greet = () =>{
+    console.log("Hi Mom!");
+}
+type SorN = string | number;
+type obj = { name: string, uid: SorN};
+
+const add = (a:number , b:number, c: SorN = 10):void =>{
+    console.log(a+b);
+    console.log(c);
+}
+add(1,2);
+const addition = (a:number , b:number):number => {
+    return a+b;
+}
+
+let value = addition(10,1);
+console.log(value);
+
