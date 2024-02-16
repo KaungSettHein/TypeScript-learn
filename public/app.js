0b496b21935eff655715237a9dfdb07571892262
Kaung Sett Hein <43985555+KaungSettHein@users.noreply.github.com>
2024-02-16 02:15:22 +0000

    class, interface, module,

--- public/app.js
+++ public/app.js
@@ -1,4 +1,23 @@
-"use strict";
+import { Invoice } from "./classes/invoice.js";
+import { Payment } from "./classes/payment.js";
+let docOne; // variable declaration
+let docTwo;
+docOne = new Invoice("name", "web work", 200);
+docTwo = new Payment("another name", "pluming work", 100);
+console.log(docOne);
+console.log(docTwo);
+let docs = [];
+docs.push(docOne);
+docs.push(docTwo);
+console.log(docs);
+// const invOne = new Invoice('someone', "website development work", 20);
+// const invTwo = new Invoice('everyone', "website development work", 80);
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// invoices.forEach(invoice => {
+//     console.log(invoice.details,invoice.amount,invoice.format());
+// })
 const form = document.querySelector(".new-item-form");
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
@@ -6,5 +25,12 @@ const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
---
+++ src/classes/invoice.ts
@@ -0,0 +1,21 @@
+import { HasFormatter } from "../interfaces/HasFormatter.js"
+
+export class Invoice implements HasFormatter {
+    // private client: string;
+    // public details: string;
+    // readonly amount: number;
+
+    // constructor(c: string, d: string, a: number) {
+    //     this.client = c;
+    //     this.details = d;
+    //     this.amount = a;
+    // }
+    constructor(
+        private client: string,
+        public details: string,
+        readonly amount: number
+    ){}
+    format(){
+        return `${this.client} owes ${this.amount}¥ for ${this.details}`;
+    }
+}
